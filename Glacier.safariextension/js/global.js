55ed91a51efd81f94acadada661859bd4bd8445a
Maurice Achtenhagen <maurice@achtenhagen.me>
2015-12-27 11:16:30 -0500

    Fix “add bookmark” UI

--- Glacier.safariextension/js/global.js
+++ Glacier.safariextension/js/global.js
@@ -1,6 +1,7 @@
 "use strict";
 
 /*
+
 var itemArray = safari.extension.toolbarItems;
 for (var i = 0; i < itemArray.length; ++i) {
     var item = itemArray[i];
@@ -8,9 +9,10 @@ for (var i = 0; i < itemArray.length; ++i) {
 
     }
 }
+
 */
 
 /*
-safari.application.addEventListener("validate", validateHandler, true);
-safari.application.addEventListener("popover", popoverHandler, true);
+	safari.application.addEventListener("validate", validateHandler, true);
+	safari.application.addEventListener("popover", popoverHandler, true);
 */
