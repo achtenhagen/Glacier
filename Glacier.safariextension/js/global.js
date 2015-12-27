9e9b4ca83975fb59a56a136e975ad306c9783c3e
Maurice Achtenhagen <maurice@achtenhagen.me>
2015-12-27 01:22:32 -0500

    Begin UI implementation

---
+++ Glacier.safariextension/js/global.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+var itemArray = safari.extension.toolbarItems;
+for (var i = 0; i < itemArray.length; ++i) {
+    var item = itemArray[i];
+    if (item.identifier == "add-bookmark") {
+
+    }
+}
+*/
+
+/*
+safari.application.addEventListener("validate", validateHandler, true);
+safari.application.addEventListener("popover", popoverHandler, true);
+*/
